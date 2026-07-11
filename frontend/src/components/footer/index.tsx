41e052e7e89d3029aef5c7c0505a6d49ef41bea2
nicz-gom <nicolasgomes.ngp@gmail.com>
2026-07-11 11:08:34 -0300

    fix: correção com componente footer

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import Fonts from "../../styles/fonts";
 
 function Footer(){
     return (
-        <footer style={{height: '18px', width: '100%', backgroundColor: Colors.Black, display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'absolute', bottom: 0, zIndex: 1}}>
+        <footer style={{height: '26px', width: '100%', backgroundColor: Colors.Black, display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'absolute', bottom: 0, zIndex: 1}}>
             <p style={{color: Colors.White, fontSize: Fonts.muito_pequeno}}>© 2026 Projeto Evol. Todos os direitos reservados - Desenvolvido por</p><a style={{color: Colors.Info, textDecoration: 'underline', fontSize: Fonts.muito_pequeno}} href="https://github.com/nicz-gom" target="_blank" rel="noopener noreferrer">: Nícolas Gomes Pacheco</a>
         </footer>
     );
